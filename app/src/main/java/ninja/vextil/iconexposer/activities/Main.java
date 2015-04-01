6dd414571e1f8aa7d1e8e97b4477ad6f814dedaa
Joaquin <vextil@gmail.com>
2015-04-01 02:47:21 -0300

    Remove "license checker"

--- app/src/main/java/ninja/vextil/iconexposer/activities/Main.java
+++ app/src/main/java/ninja/vextil/iconexposer/activities/Main.java
@@ -1,18 +1,13 @@
 package ninja.vextil.iconexposer.activities;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -33,19 +28,14 @@ import ninja.vextil.iconexposer.utilities.Preferences;
 import com.mikepenz.google_material_typeface_library.GoogleMaterial;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.accountswitcher.AccountHeader;
-import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 public class Main extends ActionBarActivity {
 
     private Drawer.Result drawerResult;
     public String thaApp, thaHome, thaPreviews, thaApply, thaWalls, thaRequest, thaCredits;
     public String version, drawerVersion;
-    private boolean firstrun, enable_features;
     private Preferences mPrefs;
-    private boolean withLicenseChecker = false;
-    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -53,7 +43,6 @@ public class Main extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        context = this;
         mPrefs = new Preferences(Main.this);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -125,8 +114,6 @@ public class Main extends ActionBarActivity {
         ).opensFragment(Credits.class);
 
         drawerResult = drawer.build();
-
-        runLicenseChecker();
 
         if (savedInstanceState == null) {
             drawerResult.setSelection(0);
@@ -203,41 +190,6 @@ public class Main extends ActionBarActivity {
         return true;
     }
 
-    public void addItemsToDrawer()
-    {
-        IDrawerItem walls = new PrimaryDrawerItem().withName(thaWalls).withIcon(GoogleMaterial.Icon.gmd_landscape).withIdentifier(4);
-        IDrawerItem request = new PrimaryDrawerItem().withName(thaRequest).withIcon(GoogleMaterial.Icon.gmd_forum).withIdentifier(5);
-        if (enable_features) {
-        //    drawerResult.addItem(walls, 3);
-         //   drawerResult.addItem(request, 4);
-        }
-    }
-
-    private void runLicenseChecker()
-    {
-        if (firstrun) {
-            if (withLicenseChecker) {
-                checkLicense();
-            } else {
-                mPrefs.setFeaturesEnabled(true);
-                addItemsToDrawer();
-                showChangelogDialog();
-            }
-        } else {
-            if (withLicenseChecker) {
-                if (!enable_features) {
-                    showNotLicensedDialog();
-                } else {
-                    addItemsToDrawer();
-                    showChangelogDialog();
-                }
-            } else {
-                addItemsToDrawer();
-                showChangelogDialog();
-            }
-        }
-    }
-
     private void changelog()
     {
         new MaterialDialog.Builder(this)
@@ -267,71 +219,7 @@ public class Main extends ActionBarActivity {
         SharedPreferences sharedPreferences = getSharedPreferences("PrefsFile", MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString("version", getResources().getString(R.string.current_version));
-        editor.commit();
-    }
-
-    public void checkLicense()
-    {
-        String installer = getPackageManager().getInstallerPackageName(getPackageName());
-        try {
-            if (installer.equals("com.google.android.feedback") ||
-                    installer.equals("com.android.vending")) {
-                new MaterialDialog.Builder(this)
-                        .title(R.string.license_success_title)
-                        .content(R.string.license_success)
-                        .positiveText(R.string.close)
-                        .callback(new MaterialDialog.ButtonCallback() {
-                            @Override
-                            public void onPositive(MaterialDialog dialog) {
-                                enable_features = true;
-                                mPrefs.setFeaturesEnabled(true);
-                                addItemsToDrawer();
-                                showChangelogDialog();
-                            }
-                        })
-                        .show();
-            } else {
-                showNotLicensedDialog();
-            }
-        } catch (Exception e) {
-            showNotLicensedDialog();
-        }
-    }
-
-    private void showNotLicensedDialog()
-    {
-        enable_features = false;
-        mPrefs.setFeaturesEnabled(false);
-        MaterialDialog dialog = new MaterialDialog.Builder(this)
-                .title(R.string.license_failed_title)
-                .content(R.string.license_failed)
-                .positiveText(R.string.download)
-                .negativeText(R.string.exit)
-                .callback(new MaterialDialog.ButtonCallback() {
-                    @Override
-                    public void onPositive(MaterialDialog dialog) {
-                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(getResources().getString(R.string.play_store_link)));
-                        startActivity(browserIntent);
-                    }
-
-                    @Override
-                    public void onNegative(MaterialDialog dialog) {
-                        finish();
-                    }
-                })
-                .show();
-        dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
-            @Override
-            public void onCancel(DialogInterface dialog) {
-                finish();
-            }
-        });
-        dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
-            @Override
-            public void onDismiss(DialogInterface dialog) {
-                finish();
-            }
-        });
+        editor.apply();
     }
 
 }
