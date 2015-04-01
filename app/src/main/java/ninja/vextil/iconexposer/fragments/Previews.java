4524df47fa7e0ab41a1c5cc5c88d877f8847438f
Joaquin <vextil@gmail.com>
2015-04-01 00:46:59 -0300

    Project restructured, package renamed and minor modifications

---
+++ app/src/main/java/ninja/vextil/iconexposer/fragments/Previews.java
@@ -0,0 +1,90 @@
+package ninja.vextil.iconexposer.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.vextil.iconexposer.R;
+import ninja.vextil.iconexposer.tabs.SlidingTabLayout;
+
+public class Previews extends Fragment
+{
+    public static Fragment newInstance(Context context)
+    {
+        return new Previews();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.section_all_icons, null);
+
+        Context context = getActivity();
+
+        ActionBar toolbar = ((ActionBarActivity) context).getSupportActionBar();
+        toolbar.setTitle(R.string.section_two);
+
+        ViewPager mPager = (ViewPager) root.findViewById(R.id.pager);
+        mPager.setAdapter(new MyPagerAdapter(getActivity().getSupportFragmentManager()));
+        SlidingTabLayout mTabs = (SlidingTabLayout) root.findViewById(R.id.tabs);
+        mTabs.setViewPager(mPager);
+        mTabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.accent);
+            }
+        });
+
+        return root;
+    }
+
+    class MyPagerAdapter extends FragmentPagerAdapter {
+
+        String[] tabs;
+
+        public MyPagerAdapter(android.support.v4.app.FragmentManager fm) {
+            super(fm);
+            tabs = getResources().getStringArray(R.array.tabs);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Fragment f = new Fragment();
+            switch (position) {
+                case 0:
+                    f = IconsFragment.newInstance(R.array.latest, R.array.latest_apps);
+                    break;
+                case 1:
+                    f = IconsFragment.newInstance(R.array.system, R.array.system_apps);
+                    break;
+                case 2:
+                    f = IconsFragment.newInstance(R.array.google, R.array.google_apps);
+                    break;
+                case 3:
+                    f = IconsFragment.newInstance(R.array.games, R.array.games_apps);
+                    break;
+                case 4:
+                    f = IconsFragment.newInstance(R.array.icon_pack, R.array.icon_pack_apps);
+                    break;
+            }
+            return f;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabs[position];
+        }
+
+        @Override
+        public int getCount() {
+            return 5;
+        }
+    }
+
+}
